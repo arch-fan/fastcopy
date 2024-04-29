33a120efda2aa4b92caf01a85ee0c6c16a7ae393
arch-fan <juanserracines@gmail.com>
2024-04-29 16:23:30 +0200

    unnecesary drizzle

--- app/server/drizzle.config.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import type { Config } from "drizzle-kit";
-
-export default {
-  schema: "./db/schema.ts",
-  out: "./drizzle",
-  driver: "better-sqlite",
-} satisfies Config;
