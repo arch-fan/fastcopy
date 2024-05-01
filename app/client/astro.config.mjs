e65bc68047b9b073768f8c84327d23358a347ed1
arch-fan <juanserracines@gmail.com>
2024-05-01 02:48:01 +0200

    changed site

--- app/client/astro.config.mjs
+++ app/client/astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  site: "https://c.arch-fan.com",
   vite: {
     server: {
       proxy: {
