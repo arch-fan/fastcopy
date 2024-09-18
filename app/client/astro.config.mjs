e7e42e8116df531bf316c153e6a3d8d26975f2b8
arch-fan <juanserracines@gmail.com>
2024-09-18 11:29:44 +0200

    Utility commands

--- app/client/astro.config.mjs
+++ app/client/astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import expressiveCode from "astro-expressive-code";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), expressiveCode()],
   site: "https://c.archfan.com",
   vite: {
     server: {
